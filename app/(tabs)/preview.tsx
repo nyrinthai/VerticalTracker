13253cbb9b8b1a04a8b572c7be97e7940e888b7d
unknown <nyrinthai@gmail.com>
2025-10-20 23:07:03 -0700

    Created the video preview feature.

---
+++ app/(tabs)/preview.tsx
@@ -0,0 +1,85 @@
+import { router, useLocalSearchParams } from 'expo-router';
+import { VideoView, useVideoPlayer } from 'expo-video';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function preview() {
+
+    const { videoUri } = useLocalSearchParams<{ videoUri: string }>();
+
+    const player = useVideoPlayer(videoUri, (player) => {
+        player.loop = true;
+        player.play();
+    });
+
+    function handleRetake() {
+        router.back();
+    }
+
+    function handleDone() {
+        router.push({
+        pathname: '/processing',
+        params: { videoUri }
+        });
+    }
+
+    return (
+        <View style={styles.container}>
+            <VideoView
+                player = {player}
+                style ={styles.video}
+                allowsFullscreen
+                allowsPictureInPicture
+                contentFit = "contain"
+                nativeControls
+                
+            />
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={handleRetake} style={styles.button}>
+                    <Text style={styles.buttonText}>Retake</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={handleDone} style={styles.button}>
+                    <Text style={styles.buttonText}>Done</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        padding : 20,
+        paddingTop : 50,
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 64,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        width: '100%',
+        paddingHorizontal: 64,
+        gap: 16,
+    },
+
+    button: {
+        backgroundColor: '#007AFF',
+        marginTop: 20,
+        marginBottom: 20,
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: '600',
+    },
+    video: {
+        width: '100%',
+        height: 500,
+    },
+})
